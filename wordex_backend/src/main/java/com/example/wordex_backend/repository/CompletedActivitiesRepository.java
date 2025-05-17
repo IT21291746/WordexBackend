e9451c843ef485bc45c7dc1659ae37f76fc372af
Namal Jayawardana <namaljayawardana2001@gmail.com>
2025-05-17 16:56:14 +0530

    commit completed activity completion

--- wordex_backend/src/main/java/com/example/wordex_backend/repository/CompletedActivitiesRepository.java
+++ wordex_backend/src/main/java/com/example/wordex_backend/repository/CompletedActivitiesRepository.java
@@ -2,8 +2,9 @@ package com.example.wordex_backend.repository;
 
 import com.example.wordex_backend.model.CompletedActivities;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
     
 
 
